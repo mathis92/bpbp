288a08eaa01da55736b36b44234ab2054c0d43a3
martinhudec <martinhudec@192.168.1.103>
2014-12-06 16:59:34 +0100

    do post + do get

--- src/main/java/sk/mathis/stuba/bpbp/MainServletVajca.java
+++ src/main/java/sk/mathis/stuba/bpbp/MainServletVajca.java
@@ -1,6 +1,15 @@
 package sk.mathis.stuba.bpbp;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonWriter;
+import javax.json.stream.JsonGenerator;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +25,10 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class MainServletVajca extends HttpServlet {
 
+    private double longitude;
+    private double latitude;
+    private JsonObject coordinatesJO;
+
     public MainServletVajca() {
 
     }
@@ -25,12 +38,47 @@ public class MainServletVajca extends HttpServlet {
         System.out.println("[POST]");
         System.out.println("REQUEST -> " + request.toString());
         System.out.println("RESPONSE -> " + response.toString());
-        
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("<h1>ššaaaak to postuje</h1>");
-        
-        
+
+        Map<String, Boolean> configMap = new HashMap<>();
+        configMap.put(JsonGenerator.PRETTY_PRINTING, Boolean.TRUE);
+
+        String coordinates = request.getParameter("coordinates");
+        System.out.println(coordinates);
+
+        System.out.println(request.getRequestURI());
+
+        if (coordinates != null) {
+            try (JsonReader jr = Json.createReader(new StringReader(coordinates))) {
+                coordinatesJO = jr.readObject();
+                JsonObject latlng = coordinatesJO.getJsonObject("coordinates");
+
+                longitude = latlng.getJsonNumber("longitude").doubleValue();
+                latitude = latlng.getJsonNumber("latitude").doubleValue();
+                System.out.println("lat = " + latitude + "\nlon = " + longitude);
+            }
+        }
+        /*
+         System.out.println("input stream -> " +is.toString());
+         JsonReaderFactory jrf = Json.createReaderFactory(configMap);
+
+         try (JsonReader jr = jrf.createReader(new InputStreamReader(is))) {
+         System.out.println("idem vypisovat");
+
+         JsonObject jo = jr.readObject();
+         ArrayList<String> config = new ArrayList<>();
+         config.add(jo.getString("latitude"));
+         System.out.println(jo.getString("latitude"));
+         config.add(jo.getString("longitude"));
+         System.out.println(config.size() + " " + config);
+         jr.close();
+
+            
+         }
+         */
     }
 
     @Override
@@ -38,10 +86,25 @@ public class MainServletVajca extends HttpServlet {
         System.out.println("[GET]");
         System.out.println("REQUEST -> " + request.toString());
         System.out.println("RESPONSE -> " + response.toString());
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>ššaaaak to klokočuje</h1>");
+        response.setContentType("text/json");
+        JsonObjectBuilder responseJob = null;
+        switch (request.getRequestURI()) {
+            case "/vehicle/39":
+                System.out.println("som v spoji 39 " + request.getRequestURI() + " " + request.getRequestURL());
+                responseJob = Json.createObjectBuilder();
+                responseJob.add("coordinates", coordinatesJO);
 
+                break;
+
+        }
+        if (responseJob != null) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            Map<String, Object> jwConfig = new HashMap<>();
+
+            jwConfig.put(JsonGenerator.PRETTY_PRINTING, true);
+            JsonWriter jw = Json.createWriterFactory(jwConfig).createWriter(response.getOutputStream());
+            jw.writeObject(responseJob.build());
+
+        }
     }
-
 }
